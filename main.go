694f7e219f21940b9945fe47e2f1adc13bf6bc23
Borrelhapje <b.a.helderman@gmail.com>
2024-03-20 09:04:20 +0000

    Fix flags parsen en db validation

--- main.go
+++ main.go
@@ -15,12 +15,16 @@ import (
 var dbpath string
 
 func init() {
-	flag.StringVar(&dbpath, "dbpath", "library.db", "Path to the sqlite database file")
+	flag.StringVar(&dbpath, "dbpath", "library.bla", "Path to the sqlite database file")
 }
 
 func main() {
+	flag.Parse()
 	db, err := sql.Open("sqlite", dbpath)
 	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err = ListItems(db); err != nil {
 		log.Fatal(err)
 	}
 	http.Handle("/list", listEndpoint(db))
@@ -42,28 +46,36 @@ type Song struct {
 
 func listEndpoint(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		rows, err := db.Query("SELECT id, path, title, added, artist, album FROM items")
+		items, err := ListItems(db)
 		if err != nil {
+			log.Default().Println(err)
 			w.WriteHeader(500)
 			return
-		}
-		defer rows.Close()
-		items := []Song{}
-		for rows.Next() {
-			var song Song
-			err := rows.Scan(&song.Id, &song.Path, &song.Title, &song.added, &song.Artist, &song.Album)
-			if err != nil {
-				w.WriteHeader(500)
-				return
-			}
-			song.EpochMillis = time.Unix(int64(song.added), 0).UnixMilli()
-			index := strings.Index(song.Path, "/complete/music")
-			song.Path = song.Path[index:len(song.Path)]
-			items = append(items, song)
 		}
 		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(200)
 		encoder := json.NewEncoder(w)
 		encoder.Encode(items)
 	}
+}
+
+func ListItems(db *sql.DB) ([]Song, error) {
+	rows, err := db.Query("SELECT id, path, title, added, artist, album FROM items")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	items := []Song{}
+	for rows.Next() {
+		var song Song
+		err := rows.Scan(&song.Id, &song.Path, &song.Title, &song.added, &song.Artist, &song.Album)
+		if err != nil {
+			return nil, err
+		}
+		song.EpochMillis = time.Unix(int64(song.added), 0).UnixMilli()
+		index := strings.Index(song.Path, "/complete/music")
+		song.Path = song.Path[index:len(song.Path)]
+		items = append(items, song)
+	}
+	return items, nil
 }
